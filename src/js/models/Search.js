42ed512cedb3736b5ba737d429bee04074c5f24c
adonayab <adonayab15@hotmail.com>
2019-06-15 17:09:07 -0500

    Shortned title of recipes, Added loading animation

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,7 +7,8 @@ export default class Search {
 
 	async getResults() {
 		const key = "50086c8429d06c41f07d3f2eff00348e";
-		const proxy = "https://cors-anywhere.herokuapp.com/";
+		// const proxy = "https://cors-anywhere.herokuapp.com/";
+		const proxy = "http://allow-any-origin.appspot.com/";
 		try {
 			const res = await axios(
 				`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${

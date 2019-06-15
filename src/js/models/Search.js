392f9a475af326648fa3a403fa651b839af537f7
adonayab <adonayab15@hotmail.com>
2019-06-15 14:36:55 -0500

    A person can search food and see the list

---
+++ src/js/models/Search.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default class Search {
+	constructor(query) {
+		this.query = query;
+	}
+
+	async getResults() {
+		const key = "50086c8429d06c41f07d3f2eff00348e";
+		const proxy = "https://cors-anywhere.herokuapp.com/";
+		try {
+			const res = await axios(
+				`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${
+					this.query
+				}`
+			);
+			this.result = res.data.recipes;
+			// console.log(this.result);
+		} catch (error) {
+			alert(error);
+		}
+	}
+}
